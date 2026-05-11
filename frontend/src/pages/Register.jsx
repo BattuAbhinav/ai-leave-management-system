b773035360dc300fde7ed3f5851f0615b7ff9fc7
BattuAbhinav <battuabhinavabhinav@gmail.com>
2026-05-11 12:09:03 +0530

    Fixed deployed URLs

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -11,7 +11,7 @@ function Register() {
     try {
 
       const response = await axios.post(
-        "http://localhost:5000/register",
+        "https://ai-leave-backend.onrender.com/register",
         {
           email,
           password
