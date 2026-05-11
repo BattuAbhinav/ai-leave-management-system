823b5e4e4d6619cc6cf323075797220f75f0e065
BattuAbhinav <battuabhinavabhinav@gmail.com>
2026-05-11 12:14:53 +0530

    Fixed deployed URLs

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -2,9 +2,7 @@ const express = require("express")
 const cors = require("cors")
 
 require("dotenv").config()
-
 const mongoose = require("mongoose")
-const jwt = require("jsonwebtoken")
 
 const User = require("./models/User")
 const Leave = require("./models/Leave")
@@ -15,7 +13,12 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => console.log(err))
 
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
+
 app.use(express.json())
 
 app.get("/", (req, res) => {
@@ -71,18 +74,8 @@ app.post("/login", async (req, res) => {
       })
     }
 
-    const token = jwt.sign(
-
-      {
-        email: user.email
-      },
-
-      "secretkey"
-    )
-
     res.json({
-      message: "Login Successful",
-      token
+      message: "Login Successful"
     })
 
   } catch (error) {
@@ -170,6 +163,8 @@ app.put("/update-leave/:id", async (req, res) => {
   }
 })
 
-app.listen(5000, () => {
-  console.log("Server Running on Port 5000")
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`)
 })
